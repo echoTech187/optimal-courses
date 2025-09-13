5d4fc119e4d5bb3d11bd18f2ae045b16f3ce8c31
Pongo <echotech8310@gmail.com>
2025-09-14 00:31:20 +0700

    update components

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -5,8 +5,14 @@ import MenuNavigation from "../navigation/page";
 
 export default function Header() {
     const [isVisible, setIsVisible] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
     useEffect(() => {
-        // document.title = "Optimal Courses";
+        document.title = "Optimal Courses";
+        function handleResize() {
+            setIsMobile(window.innerWidth <= 640);
+        }
+        window.addEventListener("resize", handleResize);
+        handleResize();
         const handleScroll = () => {
             const scrollPosition = window.scrollY;
             if (scrollPosition > window.innerHeight * 0.5) {
@@ -17,20 +23,21 @@ export default function Header() {
         };
         window.addEventListener("scroll", handleScroll);
         return () => {
+            window.removeEventListener("resize", handleResize);
             window.removeEventListener("scroll", handleScroll);
         };
     }, [isVisible]);
     return (
         <header className={`fixed top-0 left-0 z-50 w-full  py-4   transition-all ${isVisible ? 'bg-white/90 backdrop-blur-md border-b border-black/5' : 'bg-white/5 backdrop-blur-md text-white border-b border-white/10'}`}>
-            <div className="flex items-center justify-between w-full max-w-[1300px] mx-auto px-4 2xl:px-0">
-                <Brand isVisible={isVisible} />
+            <div className="flex items-center justify-start md:justify-between w-full max-w-[1300px] mx-auto px-4 2xl:px-0">
+                <Brand isVisible={isVisible} isMobile={isMobile} />
                 <MenuNavigation isVisible={isVisible} />
 
                 <div className="flex space-x-4">
                     <button className="py-2 px-4 rounded-md transition-colors">
                         Daftar
                     </button>
-                    <button className={`py-2 px-4 rounded-md transition-colors border ${isVisible ? 'border-blue-400 text-blue-400' : 'border-white text-white'} hover:bg-white hover:text-blue-600`}>
+                    <button className={`py-2 px-4 rounded-md transition-colors border ${isVisible ? 'border-purple-400 text-purple-400' : 'border-white text-white'} hover:bg-white hover:border-purple-600 hover:text-purple-600`}>
                         Masuk
                     </button>
                 </div>
---
+++ src/app/components/sidebar/mobile.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Brand from "../brand/page";
+import { useState } from "react";
+
+export default function MobileSidebar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleCloseSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+    return (
+        <aside id="mobile-sidebar" className={`fixed top-0 left-0 z-140 md:hidden h-screen overlay [--body-scroll:true] border-base-content/20 overlay-open:translate-x-0 drawer drawer-start sm:overlay-layout-open:translate-x-0 hidden w-full border-e [--auto-close:sm] [--is-layout-affect:true] [--opened:lg] sm:absolute sm:z-0 sm:flex sm:shadow-none lg:[--overlay-backdrop:false] ${isOpen ? 'block' : 'hidden'}`}>
+            <div className="drawer-header">
+                <Brand isVisible={true} isMobile={false} />
+                <button className="btn btn-circle btn-sm overlay-close lg:hidden" aria-label="Close" type="button" onClick={handleCloseSidebar}>
+                    <Icon icon="mdi:close" width="24" height="24" />
+                </button>
+            </div>
+            <div className="drawer-body px-2 h-full">
+                <nav>
+                    <ul className="menu" onClick={handleCloseSidebar}>
+                        <li >
+                            <a href="/">Beranda</a>
+                        </li>
+                        <li >
+                            <a href="#package">Layanan</a>
+                        </li>
+                        <li >
+                            <a href="#about">Tentang</a>
+                        </li>
+                        <li >
+                            <a href="#reviews">Ulasan</a>
+                        </li>
+                        <li >
+                            <a href="#articles">Artikel</a>
+                        </li>
+                        <li >
+                            <a href="#faqs">FAQ</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="drawer-footer fixed bottom-0 left-0 justify-start border-t border-base-content/20 w-full">
+                    <div className="flex space-x-4">
+                        <button className="py-2 px-4 rounded-md transition-colors border font-semibold border-purple-400 text-black hover:bg-gray-900 hover:border-gray-900 hover:text-purple-400">
+                            Daftar
+                        </button>
+                        <button className={`py-2 px-4 rounded-md transition-colors font-semibold border bg-purple-400 border-purple-400 text-black hover:bg-gray-900 hover:border-gray-900 hover:text-purple-400`}>
+                            Masuk
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </aside>
+    )
+}
