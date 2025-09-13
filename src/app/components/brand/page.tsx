b69d193ffd5bfc629ffc4945d9ea5cf6bd7d4e2e
Pongo <echotech8310@gmail.com>
2025-09-13 16:58:44 +0700

    update link logo

--- src/app/components/brand/page.tsx
+++ src/app/components/brand/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
-export default function Brand({isVisible}: {isVisible?: boolean}) {
+import Link from "next/link";
+export default function Brand({ isVisible }: { isVisible?: boolean }) {
     return (
-        <div className="text-lg font-bold flex items-center gap-2">
-            <Image src={isVisible ? "/logo.png" : "/white-logo.png"} alt="logo" width={100} height={80} className="h-12 w-fit" />
-            <Image src={isVisible ? "/Optimal.png" : "/white-optimal.png"} alt="brand" width={100} height={80} className="h-10 w-fit" />
+        <div className="text-lg font-bold">
+            <Link href={"/"} className="flex items-center gap-2">
+                <Image src={isVisible ? "/logo.png" : "/white-logo.png"} alt="logo" width={100} height={80} className="h-12 w-fit" />
+                <Image src={isVisible ? "/Optimal.png" : "/white-optimal.png"} alt="brand" width={100} height={80} className="h-10 w-fit" />
+            </Link>
         </div>
     );
 }
