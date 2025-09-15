607918fadc796d8b295800210aa5672b8ad59c0b
echotech187 <echotech187@gmail.com>
2025-09-15 08:19:33 +0700

    add ArticleBanner component with search functionality and improved content

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -149,8 +149,7 @@ export function generateStaticParams() {
             userPost: "Pradanis Yanuarinda Imkasari",
             postDate: new Date()
 
-        }]
-    console.log(posts)
+        }];
     return posts.map((post) => ({
         slug: post.slug,
         title: post.title,
